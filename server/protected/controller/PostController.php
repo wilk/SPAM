12228a7caadb16143f4e21c60a4c64f2a0ed8488
Riccardo <statuto@cs.unibo.it>
2011-05-16 22:51:56 +0200

    Fixata la comunicazione tra server nel file PostController

--- server/protected/controller/PostController.php
+++ server/protected/controller/PostController.php
@@ -7,17 +7,16 @@ class PostController extends DooController{
     }
     
     public function recivePost() {
-         $ch = curl_init(); 
-            curl_setopt($ch, CURLOPT_URL, "http://spam2.localhost:8888/post/server02/user4/post6"); 
-            curl_setopt($ch, CURLOPT_HEADER, TRUE); 
-            curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body 
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
-            $head = curl_exec($ch); 
-            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
-            curl_close($ch); 
+        /*Salvo il valore serverID dell'URI e lo stampo*/
         $server=$this->params['serverID'];
         echo ($server);
-        echo ($httpCode);
+        /*Creo una connessione ed eseguo una richiesta al server;
+         * ritorno il codice ricevuto dal server;
+         */
+        $this->load()->helper('DooRestClient');
+        $request= new DooRestClient;
+        $request->connect_to("http://spam2.localhost:8888/post/server02/user4/post6")->get();
+        return ($request->resultCode());
     }
 }
 ?>

485282e64df7f2ce8a9933ba82d29d708ca1c977
Riccardo <statuto@cs.unibo.it>
2011-05-24 22:37:40 +0200

    Aggiunto controllo user in LoginController, se c'e' restituisce 200 altrimenti 201. La funzione e' ancora da finire

--- server/protected/controller/LoginController.php
+++ server/protected/controller/LoginController.php
@@ -1,7 +1,40 @@
 <?php
 
-class LoginController extends DooController{
-  
-    public function authUser(){;}
+class LoginController extends DooController {
+
+    public function authUser() {
+        $user = strtolower($_POST['username']);
+        if ($this->firstTime($user)) {
+            //$this->addUser($user);
+            return 201;
+        } else {
+            //TODO: Crea Sessione con utente
+            return 200;
+        }
+    }
+
+    private function firstTime($user) {
+        $usersList = simplexml_load_file("users.xml");
+        foreach ($usersList->user as $myUser) {
+            if ($myUser == $user)
+                return false;
+        }
+        return true;
+    }
+
+    private function addUser($user) {
+        $this->load()->helper('DooRestClient');
+        $request = new DooRestClient;
+        $request->connect_to("/users.xml")->get();
+        $usersList= $request->xml_result();
+        $usersList->user = $user;
+        @file_put_contents("users.xml", $usersList->saveXML());
+        mkdir("data/".$user, 0777); 
+        $request->connect_to("http://vitali.web.cs.unibo.it/twiki/pub/TechWeb11/Spam/server.xml")->get();
+        $serverList = $request->xml_result();
+        @file_put_contents('data/'.$user ."/servers.xml", $serverList->saveXML());
+    }
+
 }
+
 ?>

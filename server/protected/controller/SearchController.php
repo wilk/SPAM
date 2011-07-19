60488216193bb1f3b0ebc70bdf6b6c80fc95f7c2
Riccardo Statuto <statuto@cs.unibo.it>
2011-07-20 01:26:43 +0200

    finish affinity search

--- server/protected/controller/SearchController.php
+++ server/protected/controller/SearchController.php
@@ -431,8 +431,6 @@ class SearchController extends DooController {
                                 $this->parseEXTContent3($value['data'], $arr, $tempoPostConfronto);
                             else if ($value['code'] === 500)
                                 array_push($badServer, $value['name']);
-                            else
-                                print $value['code'] . "\n\r";
 //$test[] = $value['url'].' => '.$value['code']."\n";
                         }
 //print_r($test);die();
@@ -444,9 +442,12 @@ class SearchController extends DooController {
                         return 500;
                 }
                 //print "uscito dalla richiesta..muoio!\n\r";
-                print "\n\rEcco gli articoli con rispettivi pesi(solo quelli il cui valore è positivo\n\r";
-                print_r($this->listaPost);
-                die();
+//                print "\n\rEcco gli articoli con rispettivi pesi(solo quelli il cui valore è positivo\n\r";
+//                print_r($this->listaPost);
+//                die();
+                $this->sortPost($limite);
+                $this->displayPosts();
+                break;
 
 
             default: //beh, altrimenti errore
@@ -518,6 +519,7 @@ class SearchController extends DooController {
         if (!isset($this->toMerge))
             ErrorController::internalError();
         arsort($this->toMerge, SORT_NUMERIC);
+        if ($limite != "all")
         $toRender = array_slice($this->toMerge, 0, $limite, TRUE);
         $temp = array();
         foreach ($toRender as $k => $n)
@@ -674,7 +676,10 @@ class SearchController extends DooController {
                 $realPeso = $realPeso * ($numLike - $numDislike);
             $this->listaPost[] = array(
                 "articolo" => $articolo,
-                "peso" => $realPeso,
+                "peso" => round($realPeso,5),
+            );
+            $this->toMerge[]=array(
+                "peso"=>round($realPeso,5),
             );
         }
     }
@@ -729,7 +734,7 @@ class SearchController extends DooController {
             $peso = (((($matchEsatto + ($matchParziale * 0.5))) * 1000) / ($tempo / 3600)) * ($findTerm * $findTerm);
 //                    print $peso;
 //                    print "Termini trovati $findTerm";
-            return $peso;
+            return round($peso,5);
         }
     }
 
